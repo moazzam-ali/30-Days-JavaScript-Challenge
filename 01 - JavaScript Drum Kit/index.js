ce39d6a8709b2575b9e05880dd3f329989544618
Moazzam Ali <webartist7@gmail.com>
2022-05-25 15:55:24 +0500

    some bug fixes

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,14 +1,13 @@
-window.addEventListener("keydown", e=>{
-    const audio = document.querySelector(`audio[data-key= "${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
+window.addEventListener('keydown', e=>{
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key= "${e.keyCode}"]`);
     if(!audio) return;
-    audio.currentTime=0;
     audio.play();
+    audio.currentTime = 0;
     key.classList.add('playing');
 });
 
-
 const keys = document.querySelectorAll('.key');
-keys.forEach(key=> key.addEventListener('transitionend', (e) =>{
-    key.classList.remove('playing');
+keys.forEach(key=> key.addEventListener('transitionend', e=>{
+        key.classList.remove('playing');
 }));
